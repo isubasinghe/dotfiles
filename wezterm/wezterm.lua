f4b0da4572b90194db9db9faa6fffa8911549eb8
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-06 16:29:43 +0200

    added stuff

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono",
+    "Symbols Nerd Font",
+  }),
+  color_scheme = "Dark Ocean (terminal.sexy)"
+}
